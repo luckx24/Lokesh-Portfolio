89a02b457cb54f4fa72762b98acfa7d16f7ccc7c
lucky <luckykachhi81@gmail.com>
2025-09-21 10:04:59 +0530

    Fix unescaped apostrophe in navbar

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
             className="cursor-pointer"
           />
           <span className="hidden sm:block text-white font-bold tracking-wide">
-            I'm Lokesh Kachhi
+          I&apos;m Lokesh Kachhi
           </span>
         </Link>
 
